318b7f88367b537a475c2a6cbffe899597e8ef1a
Pedro Marques dos Santos Lima <2025301705@MACEIO.IFAL>
2026-03-19 08:33:04 -0300

    feat: continuando todolist

--- index.js
+++ index.js
@@ -3,31 +3,17 @@ import path from 'path'
 
 const app = express()
 const PORT = 3000
-const pessoas = []
-const usuarios = []
+const tarefas = []
+const usuarios = {}
 
 const __dirname = import.meta.dirname
 
 app.use(express.urlencoded({extended : true}))
 app.use(express.static(path.join(__dirname, 'public')))
 
+//Lógica das tarefas
 app.get('/tarefa', (req, res) => {
     const caminho = path.join( __dirname, 'public', 'tarefa.html')
-    res.sendFile(caminho)
-})
-
-app.get('/form', (req, res) => {
-    const caminho = path.join( __dirname, 'public', 'form.html')
-    res.sendFile(caminho)
-})
-
-app.get('/cadastro', (req, res) => {
-    const caminho = path.join( __dirname, 'public', 'cadastro.html')
-    res.sendFile(caminho)
-})
-
-app.get('/', (req, res) => {
-    const caminho = path.join( __dirname, 'public', 'login.html')
     res.sendFile(caminho)
 })
 
@@ -37,8 +23,56 @@ app.post("/submit", (req, res) => {
         email: req.body.email
     }
     console.table(obj)
-    pessoas.push(obj)
-    res.redirect('/')
+    tarefas.push(obj)
+    res.redirect('/tarefa')
+})
+
+//Lógica do cadastro
+app.get('/cadastro', (req, res) => {
+    const caminho = path.join( __dirname, 'public', 'cadastro.html')
+    res.sendFile(caminho)
+})
+
+app.post("/cadastro", (req, res) => {
+
+    if ( Object.keys(usuarios).includes(req.body.email)) {
+        console.log('Já há um cadastro com esse email!')
+        return res.redirect('/cadastro')
+    } 
+
+    const user = {
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: req.body.senha
+    }
+
+    usuarios[user.email] = user
+    console.log('Usuário Cadastrado!')
+    console.table(usuarios)
+    res.redirect("/")
+})
+
+//Lógica do Login
+app.get('/', (req, res) => {
+    const caminho = path.join( __dirname, 'public', 'login.html')
+    res.sendFile(caminho)
+})
+
+app.post("/login", (req, res) => {
+
+    const user = {
+        email: req.body.email,
+        senha: req.body.senha
+    }
+
+    if (!(Object.keys(usuarios).includes(user.email))){
+        console.log('Não há usuário cadastrado com esse email!')
+    }
+
+    if ((usuarios[user.email].senha == user.senha)) {
+        console.log('Login Concluido')
+        return res.redirect('/tarefa')
+    }
 })
 
 app.listen( PORT, () => {

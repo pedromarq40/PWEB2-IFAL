91e62e96a0b8094bd287c34409be37f4910d6882
pedromarq40 <pedromarq400@gmail.com>
2026-03-23 20:45:43 -0300

    feat: API REST simples em express

--- index.js
+++ index.js
@@ -4,78 +4,100 @@ import path from 'path'
 const app = express()
 const PORT = 3000
 const tarefas = []
-const usuarios = {}
+let contador_id = 1
 
 const __dirname = import.meta.dirname
 
-app.use(express.urlencoded({extended : true}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended : true})) //permite ler os forms html usados antes
+app.use(express.static(path.join(__dirname, 'public'))) //apenas se for usar páginas html
+app.use(express.json()) // responses em json
 
-//Lógica das tarefas
+class Tarefa {
+
+    constructor(titulo, texto){
+        this.titulo = titulo
+        this.texto = texto
+        this.id = contador_id
+        this.concluida = false
+    }
+}
+
 app.get('/tarefa', (req, res) => {
-    const caminho = path.join( __dirname, 'public', 'tarefa.html')
-    res.sendFile(caminho)
+    res.status(200).json(tarefas)
 })
 
-app.post("/submit", (req, res) => {
-    const obj = {
-        tarefa: req.body.tarefa,
-        email: req.body.email
-    }
-    console.table(obj)
-    tarefas.push(obj)
-    res.redirect('/tarefa')
-})
-
-//Lógica do cadastro
-app.get('/cadastro', (req, res) => {
-    const caminho = path.join( __dirname, 'public', 'cadastro.html')
-    res.sendFile(caminho)
-})
-
-app.post("/cadastro", (req, res) => {
-
-    if ( Object.keys(usuarios).includes(req.body.email)) {
-        console.log('Já há um cadastro com esse email!')
-        return res.redirect('/cadastro')
-    } 
-
-    const user = {
-        nome: req.body.nome,
-        email: req.body.email,
-        senha: req.body.senha
+app.post("/tarefa", (req, res) => {
+   
+    const response = {
+        mensagem: "tarefa criada com sucesso!",
+        tarefas: new Tarefa(req.body.titulo, req.body.texto)
     }
 
-    usuarios[user.email] = user
-    console.log('Usuário Cadastrado!')
-    console.table(usuarios)
-    res.redirect("/")
+    contador_id++ 
+    tarefas.push(response.tarefas)
+    res.status(201).json(response)
 })
 
-//Lógica do Login
-app.get('/', (req, res) => {
-    const caminho = path.join( __dirname, 'public', 'login.html')
-    res.sendFile(caminho)
-})
+app.delete("/tarefa/:id", (req, res) => {
 
-app.post("/login", (req, res) => {
+    const id = parseInt(req.params.id)
+    const index = tarefas.findIndex( item => item.id === id)
 
-    const user = {
-        email: req.body.email,
-        senha: req.body.senha
-    }
-
-    if (!(Object.keys(usuarios).includes(user.email))){
-        console.log('Não há usuário cadastrado com esse email!')
-        return res.redirect('/')
-    }
-
-    if ((usuarios[user.email].senha == user.senha)) {
-        console.log('Login Concluido')
-        return res.redirect('/tarefa')
+    if ( index != -1){
+        const response = {
+            mensagem: "tarefa removida com sucesso!",
+            id_removido: id
+        }
+        tarefas.splice(index, 1)
+        res.status(200).json(response)
     } else {
-        console.log('Senha incorreta!')
-        return res.redirect('/')
+        const response = {
+            mensagem: "não foi encontrada uma tarefa com esse id!"
+        }
+        res.status(404).json(response)
+    }
+})
+
+app.put("/tarefa/:id", (req, res) => {
+
+    const id = parseInt(req.params.id)
+    const index = tarefas.findIndex( item => item.id === id)
+
+    if ( index != -1){
+        tarefas[index].titulo = req.body.titulo
+        tarefas[index].texto = req.body.texto
+        const response = {
+            mensagem: "tarefa substituída com sucesso!",
+            tarefa_nova : tarefas[index]
+        }
+        tarefas.splice(index, 1, response.tarefa_nova)
+        res.status(200).json(response)
+    }else{
+        const response = {
+            mensagem: "não foi encontrada uma tarefa com esse id!"
+        }
+        res.status(404).json(response)
+    }
+})
+
+app.patch("/tarefa/:id", (req, res) => {
+
+    const id = parseInt(req.params.id)
+    const index = tarefas.findIndex( item => item.id === id)
+
+    if(index != -1){
+        const response = {
+            mensagem: "status da tarefa modifcado com sucesso!",
+            tarefa: tarefas[index]
+        }
+        tarefas[index].concluida = !tarefas[index].concluida
+        res.status(200).json(response)
+
+    }else{
+        const response = {
+            mensagem: "id não encontrado"
+        }
+        res.status(404).json(response)
     }
 })
 
